2635156d53d28800cbd0d5a8ab0cf2ef518d9f42
Derek Lane <dglane001@hotmail.com>
2019-11-04 13:27:43 -0600

    adding repos for common and hedgehog

--- ShellUI/Program.cs
+++ ShellUI/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using HedgehogService.Contract;
+using MonkeyService.Contract;
+
 
 namespace ShellUI
 {
@@ -10,6 +14,13 @@ namespace ShellUI
     {
         static void Main(string[] args)
         {
+            var hedgehogProxyFactory = new ChannelFactory<IHedgehogService1>();
+            var hedgehogChannel = hedgehogProxyFactory.CreateChannel();
+            var resultHedgehog = hedgehogChannel.GetData(10);
+
+            var monkeyProxyFactory = new ChannelFactory<IMonkeyService1>();
+            var monkeyChannel = monkeyProxyFactory.CreateChannel();
+            var resultMonkey = monkeyChannel.GetData(10);
         }
     }
 }
