67e35e225e77cd2b0847956569aded7a5062c5db
Derek Lane <Derek.Lane@elekta.com>
2019-11-04 10:24:48 -0600

    adding a few fake functions to service library

---
+++ CommonLibrary/ServiceUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonServiceLibrary
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ServiceUtils
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Capitalize(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
